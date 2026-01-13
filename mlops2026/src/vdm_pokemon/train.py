5922406875404a5ea8c52eb6a5f381ffab87c00e
MilaGolomozin <s215114@dtu.dk>
2026-01-13 10:57:51 +0100

    cookiecutter file structure adopted

---
+++ mlops2026/src/vdm_pokemon/train.py
@@ -0,0 +1,10 @@
+from vdm_pokemon.model import Model
+from vdm_pokemon.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
